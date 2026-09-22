c33e1fede5d712ca2aef3111e7874f4f7ef73030
Gabrielss <gking3190@gmail.com>
2026-09-22 16:31:49 -0300

    feat: teste de herança e conclusão das partes 6 e 7 ( ͡° ͜ʖ ͡°)

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main {
-    //Não consegui inicias os testes porque o meu horário de almoço do trabalho acabou.
+    public static void main(String[] args) {
+    Aluno aluno = new Aluno("Ciências da Computação", "Gabriel Souza", "CSa01", "gabriel@a.unileste.edu.br");
+    Professor professor = new Professor("Demétrio", "CSp01", "reno@p.unileste.edu.br", "Computação");
+    FuncionarioAdministrativo funcionario = new FuncionarioAdministrativo("Dorinha", "Adm01", "Dorinha@f.unileste.edu.br", "Bibliotecário");
+
+    aluno.exibirDados();   
+    professor.exibirDados();   
+    funcionario.exibirDados();   
+    }
 }
